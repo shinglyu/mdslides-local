d590641bc3b335be24cf6231a974b8fbc4d21f0a
Shing Lyu <slyu@mozilla.com>
2014-09-30 23:07:22 +0800

    Fixed bug in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,17 @@ pushd $script_dir
 source config.sh
 dboxpublic=$HOMEDIR/Dropbox/Public
 
-tmpdir=$dboxpublic/slides$(date +%s%N |md5sum |cut -c 1-6)
-cp -r . $tmpdir
+tmpdir=slides$(date +%s%N |md5sum |cut -c 1-6)
+fulltmpdir=$dboxpublic/$tmpdir
+rsync -r --exclude=.git --exclude=*.sh . $fulltmpdir
 popd
 
-cp $1 $tmpdir/tmp.md
+cp $1 $fulltmpdir/tmp.md
 #echo "$content"
-pushd $tmpdir
-sed -e "/Your content will be here./r tmp.md" -e "//d" $tmpdir/template.html> $tmpdir/$1.html 
+pushd $fulltmpdir
+sed -e "/Your content will be here./r tmp.md" -e "//d" $fulltmpdir/template.html> $fulltmpdir/$1.html 
 popd
 
-echo "Your slides is published to ",
+echo "Your slides are published to:"
 echo "$DBOXPREFIX/$tmpdir/$1.html"
 
