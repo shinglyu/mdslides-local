73ea079f722422346fd787b59f7645b0c841eee5
Shing Lyu <shing.lyu@gmail.com>
2015-02-21 17:25:21 +0800

    Fixed deploy

--- bin/config.sh
+++ bin/config.sh
@@ -1,3 +1,6 @@
 dropbox_public_dir="$HOME/Dropbox/Public"
 dropbox_public_url_prefix='https://dl.dropboxusercontent.com/u/7281903'  #common prefix for your Dropbox folder
 
+local_lib_dir=/tmp/mdslides
+public_lib_dir=$dropbox_public_dir/mdslides
+
