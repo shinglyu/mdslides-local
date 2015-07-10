61c4642bdc3735086af82f03e15038183967884b
Shing Lyu <shing.lyu@gmail.com>
2015-07-11 06:55:33 +0800

    Minimal gulp build

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+var inject = require('gulp-inject');
+var debug = require('gulp-debug');
+var replace = require('gulp-replace');
+var copy = require('gulp-copy');
+var gulp_filter = require('gulp-filter');
+var path = require('path');
+var del = require('del')
+var open = require('gulp-open')
+
+var md_path = process.env.INIT_CWD //#the initial directory
+var gulpfile_path = process.cwd() //#gulpfile.js location
+
+gulp.task('default', ['clean'], function() {
+  
+
+  //gulp.src(path.join(md_loc, '*')).pipe(debug())
+  //gulp.src(path.join(gulpfile_path, 'custom', '**', '*')).pipe(debug())
+  //gulp.src(path.join(gulpfile_path, 'custom', '**', '*')).pipe(debug())
+  //Configureing destination
+  var dest_path = path.join(md_path, 'dist')
+  var dest_lib_path = path.join(dest_path, 'mdslides')
+  var dest = gulp.dest(dest_path)
+
+
+  //Copying lib files => Should be removed after webpack is ready
+  gulp.src([path.join(gulpfile_path, 'bower_components', 'reveal.js', '**', '**', '*'),
+            path.join(gulpfile_path, 'custom', '**', '*')])
+      .pipe(gulp.dest(path.join(dest_lib_path)))
+
+  gulp.src([path.join(gulpfile_path, 'bower_components', 'jquery', 'dist', 'jquery.min.js')])
+      .pipe(gulp.dest(path.join(dest_lib_path, 'lib', 'js')))
+
+  //Sources
+  var exclude = gulp_filter(['*','!README.md']);
+  var content = gulp.src([path.join(md_path, '*.md')])
+                    .pipe(exclude)
+
+  var template = gulp.src(path.join(gulpfile_path, 'custom', 'template.html'))
+
+  //Injecting the content into the template
+  template.pipe(inject(content,{
+      transform: function (filePath, file) {
+         return file.contents.toString('utf8')
+      }
+    }))
+    .pipe(replace(/<!-- inject:md -->/g, ''))
+    .pipe(replace(/<!-- endinject -->/g, ''))
+    .pipe(dest)
+    .pipe(open('<%=file.path%>', {app: 'firefox'}))
+    //.pipe(debug())
+
+})
+
+gulp.task('clean', function(cb) {
+  del([path.join(md_path, 'dist')], {'force': true}, cb)
+})
